40d7d1775bbb618f68fa4ed5477030ec28d705ef
Ducksan Cho <ducktyper@gmail.com>
2017-05-20 00:04:27 +1200

    Save time, app, branch, and tmux window name to log

--- collect.sh
+++ collect.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 LOG="collect.log"
+PROJECT_PATH="/Users/ducksan/projects/hermes"
 
 if [ ! -e $LOG ]; then
   touch $LOG
 fi
 
-echo "hello" >> $LOG
+TIME=$(date '+%F %T')
+APP=$(osascript -e 'tell application "System Events" to get name of (processes where frontmost is true)')
+BRANCH=$(cd $PROJECT_PATH && git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+TMUX=$(tmux display-message -p '#W')
+
+echo "$TIME,$APP,$BRANCH,$TMUX" >> $LOG
